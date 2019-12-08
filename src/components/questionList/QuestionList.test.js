7a21262a9d31a06c070a684b3b85b334df6664bf
sujaymohithe <sujay.rao.mohithe@accenture.com>
2019-12-08 21:32:39 +0530

    Added test case for Question List component

--- src/components/questionList/QuestionList.test.js
+++ src/components/questionList/QuestionList.test.js
@@ -5,7 +5,7 @@ import Adapter from 'enzyme-adapter-react-16';
 import QuestionListHome from './QuestionListContainer';
 import Question from './Question';
 import { Provider } from "react-redux";
-import {store} from '../../store';
+import { store } from '../../store';
 import App from '../../App';
 
 Enzyme.configure({ adapter: new Adapter() });
@@ -32,7 +32,25 @@ describe('Question List Container And Question Widget Component', () => {
             <Provider store={store} >
                 <App />
             </Provider>);
-        console.log(wrapper.debug());
         expect(wrapper.find('.Boxwidget').length).toEqual(0);
+    })
+
+    it('Should render Question widget in Question List Home Page on setting Props', () => {
+        const wrapper = mount(
+            <Provider store={store}>
+                <QuestionListHome />
+            </Provider>);
+        wrapper.setProps({isFetching : false});
+        wrapper.setProps({ questions: [{ "question": "Books", "published_at": "2019-12-08T14:37:58.635561+00:00", "url": "/questions/13391", "choices": [{ "choice": "Book 1", "votes": 0, "url": "/questions/13391/choices/54340" }] }] });
+        expect(wrapper.find(".Row").length).toEqual(1);
+    })
+
+    it('renders Create Component link', () => {
+        const component = mount(
+            <Provider store={store} >
+                <QuestionListHome />
+            </Provider>);
+        const wrapper = component.find('.ButtonPrimary').first();
+        expect(wrapper.text() == "Create New Question");
     })
 })
